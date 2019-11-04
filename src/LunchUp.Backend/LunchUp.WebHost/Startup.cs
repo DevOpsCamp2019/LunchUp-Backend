516df2abfae46ab6a973fd7321d475abe3b58f9a
Jonas Gschwend <Jonas.Gschwend@zuehlke.com>
2019-11-04 15:00:59 +0100

    Add and enable Swagger for Api Documentation

--- src/LunchUp.Backend/LunchUp.WebHost/Startup.cs
+++ src/LunchUp.Backend/LunchUp.WebHost/Startup.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
 
 namespace LunchUp.WebHost
 {
@@ -12,6 +13,18 @@ namespace LunchUp.WebHost
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore()
+                .AddApiExplorer();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                { 
+                    Title = "LunchUp API",
+                    Version = "V1"
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,14 +35,18 @@ namespace LunchUp.WebHost
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LunchUp API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
