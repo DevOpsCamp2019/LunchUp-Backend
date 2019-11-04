5f615d3a493674ba762e926958c4145ab2bda649
Jonas Gschwend <Jonas.Gschwend@zuehlke.com>
2019-11-04 17:17:09 +0100

    Add simple model

---
+++ src/LunchUp.Backend/LunchUp.Model/PersonEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace LunchUp.Model
+{
+    public class PersonEntity
+    {
+        public Guid Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public MailAddress Email { get; set; }
+        public string Photo { get; set; }
+    }
+}
---
+++ src/LunchUp.Backend/LunchUp.Model/ResponseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LunchUp.Model
+{
+    public class ResponseEntity
+    {
+        public PersonEntity Origin { get; set; }
+        public PersonEntity Target { get; set; }
+        public bool Like { get; set; }
+        public DateTime ResponseDate { get; set; }
+    }
+}
