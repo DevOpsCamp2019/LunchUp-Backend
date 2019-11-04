49bc777480a2ca442bd1975bd1b7666e8ff07e28
Jonas Gschwend <Jonas.Gschwend@zuehlke.com>
2019-11-04 16:31:17 +0100

    Remove unused using

--- src/LunchUp.Backend/LunchUp.WebHost/Startup.cs
+++ src/LunchUp.Backend/LunchUp.WebHost/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Interfaces;
 using Microsoft.OpenApi.Models;
 
 
