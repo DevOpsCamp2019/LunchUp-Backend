96fefaeb5b56d0a1eaac98540762ac42af7104ba
Jonas Gschwend <Jonas.Gschwend@zuehlke.com>
2019-11-05 10:21:48 +0100

    Add IntegrationController and refactor SuggestionController

---
+++ src/LunchUp.Backend/LunchUp.WebHost/Controller/IntegrationController.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using LunchUp.WebHost.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LunchUp.WebHost.Controller
+{
+    /// <inheritdoc />
+    [Route("api/integration")]
+    [ApiController]
+    public class IntegrationController : ControllerBase
+    {
+        /// <inheritdoc />
+        public IntegrationController() {  }
+
+        /// <summary>
+        /// Create a person
+        /// </summary>
+        [HttpPut]
+        [Route("person")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public Task CreateOrUpdatePerson([FromBody][Required] Person person)
+        {
+            return Task.FromResult(StatusCodes.Status200OK);
+        }
+
+    }
+}
