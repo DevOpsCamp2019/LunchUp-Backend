70f95d95017abf4fc3d07fd3836d9a7dd11217a6
Samuel Meier <samuel.meier@zuehlke.com>
2019-11-07 16:12:04 +0100

    add logging

--- src/LunchUp.Backend/LunchUp.Model/LunchUpContext.cs
+++ src/LunchUp.Backend/LunchUp.Model/LunchUpContext.cs
@@ -1,12 +1,24 @@
 ﻿using LunchUp.Model.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
 
 namespace LunchUp.Model
 {
     public class LunchUpContext : DbContext
     {
-        public LunchUpContext(DbContextOptions<LunchUpContext> options) : base(options)
+        private ILoggerFactory _loggerFactory;
+
+        public LunchUpContext(DbContextOptions<LunchUpContext> options, ILoggerFactory loggerFactory) : base(options)
         {
+            _loggerFactory = loggerFactory;
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(_loggerFactory) //tie-up DbContext with LoggerFactory object
+                .EnableSensitiveDataLogging();
         }
 
         public DbSet<PersonEntity> Person { get; set; }
--- src/LunchUp.Backend/LunchUp.WebHost/Startup.cs
+++ src/LunchUp.Backend/LunchUp.WebHost/Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
