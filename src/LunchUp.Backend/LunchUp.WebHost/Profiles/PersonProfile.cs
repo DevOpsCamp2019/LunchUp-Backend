42fab0b37041023cc7787cf0f4d558ec3c611db1
Samuel Meier <samuel.meier@zuehlke.com>
2019-11-07 09:57:01 +0100

    add xml comments

--- src/LunchUp.Backend/LunchUp.WebHost/Profiles/PersonProfile.cs
+++ src/LunchUp.Backend/LunchUp.WebHost/Profiles/PersonProfile.cs
@@ -5,6 +5,7 @@ using LunchUp.WebHost.Dto;
 namespace LunchUp.WebHost.Profiles
 {
     /// <inheritdoc />
+    // ReSharper disable once UnusedMember.Global
     public class PersonProfile : Profile
     {
         /// <inheritdoc />
